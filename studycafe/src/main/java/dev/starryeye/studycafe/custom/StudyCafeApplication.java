40c100b6c71443f314397ddcf25fce566379d46a
starryeye <see2818@naver.com>
2024-08-06 20:54:47 +0900

    [starryeye, 2024.08.06] - studycafe init
    내용
    - studycafe init

---
+++ studycafe/src/main/java/dev/starryeye/studycafe/custom/StudyCafeApplication.java
@@ -0,0 +1,10 @@
+package dev.starryeye.studycafe.custom;
+
+public class StudyCafeApplication {
+
+    public static void main(String[] args) {
+        StudyCafePassMachine studyCafePassMachine = new StudyCafePassMachine();
+        studyCafePassMachine.run();
+    }
+
+}
